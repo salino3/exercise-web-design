1314a1cc362d5471e896b872b668f6ed8bd6c4f5
salino3 <programacionflavio@gmail.com>
2023-12-25 10:20:40 +0100

    add two lists

--- src/app.js
+++ src/app.js
@@ -15,8 +15,74 @@ const formHeader = document.querySelector("#form-1");
 
 formHeader.appendChild(FormField.headerField.render());
 
-// const containerForm = document.querySelector(".form-group-1");
-
-
 formHeader.appendChild(Button.headerBtn.render());
+
+// 
+
+const box1 = document.querySelector('.longUp');
+
+let item = "cars";
+
+
+const firstUl = document.createElement('ul');
+firstUl.classList.add('list-1');
+
+const firstLiArray = vehicles.types.map((currentValue) => {
+    const my_li = document.createElement('li');
+    
+    my_li.innerText += currentValue;
+    my_li.addEventListener('click', function() {
+              item = currentValue.toLowerCase();
+
+                while (secondUl.firstChild) {
+                  secondUl.removeChild(secondUl.firstChild);
+                }
+
+                 // Crear nueva lista basada en el nuevo item
+                vehicles[item].types.forEach((type) => {
+                   const type_li = document.createElement("li");
+                   type_li.innerText = type;
+                   secondUl.appendChild(type_li);
+                });
+    });
+
+    return my_li;
+ }
+);
+
+console.log("aqui", firstLiArray)
+
+firstLiArray.forEach((li) => {
+    firstUl.appendChild(li);
+});
+
+
+box1.appendChild(firstUl);
+
+// 
+
+const verticalLine1 = document.createElement('hr');
+verticalLine1.classList.add("firstLine");
+
+box1.appendChild(verticalLine1)
+
+// 
+
+const secondUl = document.createElement("ul");
+secondUl.classList.add("list-2");
+
+const secondLiArray = vehicles[item].types.map((currentValue) => {
+  const my_li = document.createElement("li");
+
+  my_li.innerText += currentValue;
+
+  return my_li;
+});
+
+secondLiArray.forEach((li) => {
+  secondUl.appendChild(li);
+});
+
+box1.appendChild(secondUl);
+
 
